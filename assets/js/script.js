602251e9160e545e386b477d1f5400c84ad51e98
Shaun Greene <shaunmichaelgreene@gmail.com>
2022-05-03 19:07:04 -0400

    blockShading and refreshShading functions refactored, color-coding functional

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,66 +1,54 @@
-//define global variables
 var descriptionEl = document.querySelector(".description");
-console.log(descriptionEl);
-console.log(descriptionEl.parentElement.id);
 
-var currentTime = new Date(); //gets current time from browser (will need to make this account for time zones)
-console.log("the current time is: " + currentTime);
-var currentHour = currentTime.getHours()
-console.log("the current hour of the day is: " + currentHour); //this identifies the current hour as a numerical value
-
-var timeShader = function() {
-    var blockValue = document.querySelector(".description")
-    // console.log(blockValue.parentElement.id);
-    var currentTime = new Date();
-    var currentHour = currentTime.getHours()
-    // var timeBlockValue = $(this).closest('div').attr('id');
-    // console.log($(this).parent('div'));
-    // console.log("the current time-block being checked has an ID of: " + timeBlockValue);
-    if (blockValue < currentHour) {
-        $(descriptionEl).addClass("past");
-    } else if (blockValue > currentHour) {
-        $(descriptionEl).addClass("future");
-    } else if (blockValue == currentHour) {
-        $(descriptionEl).addClass("present");
-    }
-}
-
-setInterval(function () {
-    $(".description").each(function(i, descriptionEl) {
-        console.log(descriptionEl);
-      
-    });
-}, 1500);
-
-// console.log($(".description"));
-
-timeShader();
-//syntax for targeting 2nd child: $(target).children("div.second")
-
-var initialShading = function() {
-    //reset all classes
-    for (let i=9; i <=17; i++) {
-        // console.log("the current value of i is: " + i);
-        var initialBlock = document.getElementById(i);
-        var timeValue = document.getElementById(i).id;
-        // console.log("the current value of the timeValue id is: " + timeValue);
-        // console.log("same, but using JQ: " + $(initialBlock).children("div.second"));
-        var initialTime = new Date();
-        var initialHour = initialTime.getHours();
-        // console.log(initialBlock.children);
-        if (timeValue < initialHour) {
-            $(initialBlock.children[1]).addClass("past");
-        } else if (timeValue > initialHour) {
-            $(descriptionEl).addClass("future");
-        } else if (timeValue == initialHour) {
-            $(descriptionEl).addClass("present");
-        }
-    }
+var classRemover = function(blockValue) {
+    $(blockValue.children[1]).removeClass("past");
+    $(blockValue.children[1]).removeClass("future");
+    $(blockValue.children[1]).removeClass("present");
+    console.log("the classRemover function has stripped a class");
 };
 
-//setter function? - sets/removes classes as needed
+var blockShading = function() { //handles initial shading of the time blocks
+    var currentTime = new Date(); //gets current time from browser
+    var currentHour = currentTime.getHours(); //this identifies the current hour as a numerical value
+    for (let i=9; i <=17; i++) {
+        var blockValue = document.getElementById(i);
+        var timeValue = document.getElementById(i).id;
+        classRemover(blockValue); //reset the time-based classes
+        if (timeValue < currentHour) {
+            $(blockValue.children[1]).addClass("past"); //if the value of the target time block is less than the value of the current hour (0-23), then the target time block is in the past, corresponding class added
+        } else if (timeValue > currentHour) {
+            $(blockValue.children[1]).addClass("future"); //if value of target block is greater than current hour, time block is in the future, corresponding class added
+        } else if (timeValue == currentHour) {
+            $(blockValue.children[1]).addClass("present"); //if value of target block is equal to the current hour, time block is in the present, corresponding class added
+        };
+    };
+};
 
-initialShading();
+blockShading();
+
+setInterval(function () {
+    $(".description").each(function(i, descriptionEl) { //loops through each Time Block Description element (9AM-5PM)
+        refreshShading(descriptionEl); //passes element to refreshShading function
+    });
+}, 60000); //refreshes every minute to update block shading as needed
+
+var refreshShading = function(blockDescription) {
+    var blockValue = blockDescription;
+    var timeValue = blockDescription.parentElement.id;
+    var currentTime = new Date();
+    var currentHour = currentTime.getHours();
+    classRemover(blockValue);
+    if (timeValue < currentHour) {
+        $(blockValue.children[1]).addClass("past");
+    } else if (timeValue > currentHour) {
+        $(blockValue.children[1]).addClass("future");
+    } else if (timeValue == currentHour) {
+        $(blockValue.children[1]).addClass("present");
+    };
+};
+
+
+
 // console.log(current.getHours()); 
 
 // if (moment().isAfter(currentTime)) {
@@ -81,3 +69,12 @@ initialShading();
 //4. loop continuously
 //5. reset each day at midnight
 
+//CODE GRAVEYARD
+// console.log($(".description"));
+//syntax for targeting 2nd child: $(target).children("div.second")
+
+//     // console.log(blockValue.parentElement.id);
+//     // var timeBlockValue = $(this).closest('div').attr('id');
+//     // console.log($(this).parent('div'));
+//     // console.log("the current time-block being checked has an ID of: " + timeBlockValue);
+// }
